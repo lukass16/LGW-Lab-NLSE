ed3d3b20eb4eb30573ea9ea28e796be3e0774738
lukass16 <lukasskellijs@gmail.com>
2026-04-21 14:45:43 -0400

    Add Batch Jobs

--- NLSE in PyTorch/job_cluster_test.sh
+++ NLSE in PyTorch/job_cluster_test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 #SBATCH --job-name=xpm_train_gpu
-#SBATCH --output=/home/lrk42/"LGW Lab"/unitary/LGW-Lab-NLSE/"NLSE in PyTorch"/runs/slurm_%j_out.txt
-#SBATCH --error=/home/lrk42/"LGW Lab"/unitary/LGW-Lab-NLSE/"NLSE in PyTorch"/runs/slurm_%j_err.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus=1
-#SBATCH --partition=gpu
+#SBATCH --partition=gpu_rtx6000
 #SBATCH --time=1:00:00
 
 export WANDB_API_KEY=fa6aa2781732fe60c4a13ca88d919b7f56360fd8
@@ -17,4 +15,4 @@ module load CUDA
 module load cuDNN
 module load miniconda
 conda activate torch-gpu
-python train_xpm_unitary.py --config configs/analysis/trace-spm-off/bs16.yaml
+python train_xpm_unitary.py --config configs/analysis/trace-spm-off/bs4.yaml
--- NLSE in PyTorch/priority.sh
+++ NLSE in PyTorch/priority.sh
@@ -8,7 +8,7 @@
 #SBATCH --gpus=rtx_5000_ada:1
 #SBATCH --partition=priority_gpu
 #SBATCH --account=prio_lgw23
-#SBATCH --mem=32G
+#SBATCH --mem=5G
 #SBATCH --time=2:00:00
 
 export WANDB_API_KEY=fa6aa2781732fe60c4a13ca88d919b7f56360fd8
@@ -19,4 +19,4 @@ module load CUDA
 module load cuDNN
 module load miniconda
 conda activate torch-gpu
-python train_xpm_unitary.py --config configs/hg-loss/adam_lr5e-2.yaml
+python train_xpm_unitary.py --config configs/analysis/trace-spm-off/bs8.yaml
