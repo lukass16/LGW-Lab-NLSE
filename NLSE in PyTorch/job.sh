f0fb249594629154f2482a12c1ad61a2743c50e1
lukass16 <lukasskellijs@gmail.com>
2026-04-21 14:52:40 -0400

    Update Script Path for Bouchet

--- NLSE in PyTorch/job.sh
+++ NLSE in PyTorch/job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=xpm_train_gpu
-#SBATCH --output=/home/lrk42/"LGW Lab"/unitary/LGW-Lab-NLSE/"NLSE in PyTorch"/runs/slurm_%j_out.txt
-#SBATCH --error=/home/lrk42/"LGW Lab"/unitary/LGW-Lab-NLSE/"NLSE in PyTorch"/runs/slurm_%j_err.txt
+#SBATCH --output=/home/lrk42/LGW-Lab-NLSE/"NLSE in PyTorch"/runs/slurm_%j_out.txt
+#SBATCH --error=/home/lrk42/LGW-Lab-NLSE/"NLSE in PyTorch"/runs/slurm_%j_err.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus=1
@@ -11,7 +11,7 @@
 
 export WANDB_API_KEY=fa6aa2781732fe60c4a13ca88d919b7f56360fd8
 
-cd /home/lrk42/"LGW Lab"/unitary/LGW-Lab-NLSE/"NLSE in PyTorch"
+cd /home/lrk42/LGW-Lab-NLSE/"NLSE in PyTorch"
 
 module load CUDA
 module load cuDNN
